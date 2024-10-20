bd1b7eb3011d27919a4f7791b10314e48175b6f1
HUECYCLES <luhlinard@gmail.com>
2024-10-19 21:55:37 -0300

    updated faq page

--- js/components/nav.js
+++ js/components/nav.js
@@ -49,9 +49,9 @@ class MyNav extends HTMLElement {
             </style>
             <nav>
                 <li><a href="./chapters" onclick="clickSoundLoad(event)" onmouseenter="hoverSound()">Home</a></li>
-                <li><a href="#0" onclick="clickDenySound()" onmouseenter="heartBreak()" class="break">Team</a></li>
-                <li><a href="#0" onclick="clickDenySound()" onmouseenter="heartBreak()" class="break">FAQ</a></li>
-                <li><a href="#0" onclick="clickDenySound()" onmouseenter="heartBreak()" class="break">Newsletters</a></li>
+                <li><a href="./team" onclick="clickSoundLoad(event)" onmouseenter="hoverSound()">Team</a></li>
+                <li><a href="./faq" onclick="clickSoundLoad(event)" onmouseenter="hoverSound()">FAQ</a></li>
+                <li><a href="./newsletters" onclick="clickSoundLoad(event)" onmouseenter="hoverSound()">Newsletters</a></li>
             </nav>
         `;
     }
