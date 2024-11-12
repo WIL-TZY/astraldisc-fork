8438e60edf531b90924bacbc01fcbbe4f6f5858d
Nael <willowitzy@gmail.com>
2024-11-12 10:15:18 -0300

    improved ux with menu selection in character pages

--- js/tabs.js
+++ js/tabs.js
@@ -1,13 +1,23 @@
-const tabs = document.querySelectorAll(".tab-btn");
+const tab_buttons = document.querySelectorAll(".tab-btn");
+const profiles = document.querySelectorAll(".character-profile");
 
-tabs.forEach(tab => tab.addEventListener("click", () => tabClicked(tab)));
-
-const tabClicked = (tab) => {
-    const contents = document.querySelectorAll(".content");
-    contents.forEach(content => content.classList.remove("show"));
-
-    const contentId = tab.getAttribute("content-id");
-    const content = document.getElementById(contentId);
-
-    content.classList.add("show");
+function clearActiveClasses() {
+    tab_buttons.forEach(btn => btn.classList.remove("active"));
+    profiles.forEach(profile => profile.classList.remove("active"));
 }
+
+function selectCharacter(event) {
+    const characterId = event.currentTarget.getAttribute("data-character");
+
+    clearActiveClasses(); // Clear active classes from previous selection
+
+    // Add active class to the selected button and profile
+    event.currentTarget.classList.add("active");
+    document.getElementById(characterId).classList.add("active");
+}
+
+// Add event listeners
+tab_buttons.forEach(button => { button.addEventListener("click", selectCharacter); });
+
+// Select the first character by default on page load
+tab_buttons[0].click();
